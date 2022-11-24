3a9e95bf81b74c80acdafb9a124a31ac37eff721
Supercrazysquid <supercrazysquid@gmail.com>
2022-11-24 18:51:06 -0500

    Succesfully added room switching

--- src/bot.py
+++ src/bot.py
@@ -25,8 +25,11 @@ token = config['token']
 
 rooms = []
 
+main_guild = 1041055949571498036
+
 # Setup discord bot
-bot: commands.bot.Bot = commands.bot.Bot(command_prefix="!", intents=discord.Intents.all())
+client = discord.Client(intents=discord.Intents.all())
+tree = discord.app_commands.tree.CommandTree(client)
 
 # Functions
 def create_room(name, permission):
@@ -35,24 +38,62 @@ def create_room(name, permission):
 	rooms.append(newRoom)
 
 # Commands
-@bot.command(name='rooms')
-async def _rooms(ctx: commands.Context):
+@tree.command(guild=discord.Object(id=main_guild), name='rooms', description='Sends all the rooms')
+async def _rooms(interaction: discord.Interaction):
 	roomsOut = ''
 
 	for room in rooms:
 		roomsOut += str(room.roomName) + '\n'
 
-	await ctx.reply('The following rooms are avaliable```\n' + roomsOut + '```')
+	await interaction.response.send_message('The following rooms are available```\n' + roomsOut + '```')
+
+@tree.command(guild=discord.Object(id=main_guild), name='gotoroom', description='Sends user to a different room')
+async def _gotoroom(interaction: discord.Interaction, room: str):	
+	roles = []
+	author = interaction.user
+
+	room = room.lower()
+
+	gotoRoom = None
+
+	# Getting room the user inputted and logging all the roles
+	for roomClass in rooms:
+		if roomClass.roomName == room:
+			gotoRoom = roomClass
+
+		roleId = roomClass.requiredPermission
+		roles.append(discord.utils.get(author.guild.roles, id=roleId))
+
+	# Getting group needed for room
+	roomPermission = gotoRoom.requiredPermission
+	roomRole = discord.utils.get(author.guild.roles, id=roomPermission)
+
+	# Clearing room permissions
+	for role in roles:
+		await author.remove_roles(role)
+
+	# Adding the main room's permissions
+	await author.add_roles(roomRole)
+
+	# Sending response
+	await interaction.response.send_message('Successfully went to room, ' + gotoRoom.roomName)
+
 
 # Setting up rooms
-create_room('LivingRoom', 1041057800090038393)
-create_room('Kitchen', 1041057809225240686)
-create_room('Bedroom', 1041057809946652792)
+create_room('livingroom', 1041057800090038393)
+create_room('kitchen',    1041057809225240686)
+create_room('bedroom',    1041057809946652792)
 
 # Events
-@bot.event
+@client.event
 async def on_ready():
-	print(bot.user.name, 'is ready!')
+	print('Waiting until the client is ready')
+	await client.wait_until_ready()
+
+	print('Syncing commands')
+	await tree.sync(guild=discord.Object(id=main_guild))
+
+	print(client.user.name, 'is ready!')
 
 # Bot login
-bot.run(token)
+client.run(token)
