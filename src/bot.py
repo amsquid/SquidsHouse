8b3237098d6bee1b884d7a1606813451d322feb8
Supercrazysquid <supercrazysquid@gmail.com>
2022-11-24 17:47:54 -0500

    Bot working with !rooms command

--- src/bot.py
+++ src/bot.py
@@ -1,40 +1,58 @@
 # Imports
 import discord
 
-from discord import app_commands
 from discord.ext import commands
-
-import Room from "./room"
+from room import Room
 
 # Get data from config.dat
 config = {}
 
 with open('./config.dat', 'r') as configFile:
-    data = configFile.read()
+	data = configFile.read()
 
-    lines = data.split(' ')
+	lines = data.split(' ')
 
-    for line in lines:
-        config_item = line.split('=')
+	for line in lines:
+		config_item = line.split('=')
 
-        key = str(config_item[0])
-        value = str(config_item[1].replace('\n', ''))
-
-        print(key + "=" + value)
-        
-        config[key] = value
+		key = str(config_item[0])
+		value = str(config_item[1].replace('\n', ''))
+		
+		config[key] = value
 
 # Variables
 token = config['token']
 
+rooms = []
+
 # Setup discord bot
-bot = commands.bot(command_prefix="!", intents = discord.Intents.all())
+bot: commands.bot.Bot = commands.bot.Bot(command_prefix="!", intents=discord.Intents.all())
+
+# Functions
+def create_room(name, permission):
+	newRoom = Room(name, permission)
+
+	rooms.append(newRoom)
 
 # Commands
-@bot.command()
-def help(ctx: discord):
-    ctx.
+@bot.command(name='rooms')
+async def _rooms(ctx: commands.Context):
+	roomsOut = ''
+
+	for room in rooms:
+		roomsOut += str(room.roomName) + '\n'
+
+	await ctx.reply('The following rooms are avaliable```\n' + roomsOut + '```')
+
+# Setting up rooms
+create_room('LivingRoom', 1041057800090038393)
+create_room('Kitchen', 1041057809225240686)
+create_room('Bedroom', 1041057809946652792)
 
 # Events
+@bot.event
+async def on_ready():
+	print(bot.user.name, 'is ready!')
 
 # Bot login
+bot.run(token)
--- src/room.py
+++ src/room.py
@@ -1,6 +1,5 @@
 class Room:
-    def __init__(self, roomName, requiredPermission, channelID):
+    def __init__(self, roomName, requiredPermission):
         self.roomName = roomName
         self.requiredPermission = requiredPermission
-        self.channelID = channelID
         
