77ffccb8fcc2831e4adb86ba6442dc04f7daed27
Supercrazysquid <supercrazysquid@gmail.com>
2022-11-24 17:00:48 -0500

    Added command bot

--- src/bot.py
+++ src/bot.py
@@ -1,2 +1,40 @@
-import discord-py-slash-command
+# Imports
+import discord
 
+from discord import app_commands
+from discord.ext import commands
+
+import Room from "./room"
+
+# Get data from config.dat
+config = {}
+
+with open('./config.dat', 'r') as configFile:
+    data = configFile.read()
+
+    lines = data.split(' ')
+
+    for line in lines:
+        config_item = line.split('=')
+
+        key = str(config_item[0])
+        value = str(config_item[1].replace('\n', ''))
+
+        print(key + "=" + value)
+        
+        config[key] = value
+
+# Variables
+token = config['token']
+
+# Setup discord bot
+bot = commands.bot(command_prefix="!", intents = discord.Intents.all())
+
+# Commands
+@bot.command()
+def help(ctx: discord):
+    ctx.
+
+# Events
+
+# Bot login
