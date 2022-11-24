665ca57427da7881e49abee1ef814f2ecc524ba9
Supercrazysquid <supercrazysquid@gmail.com>
2022-11-24 17:00:58 -0500

    Added room system

---
+++ src/room.py
@@ -0,0 +1,6 @@
+class Room:
+    def __init__(self, roomName, requiredPermission, channelID):
+        self.roomName = roomName
+        self.requiredPermission = requiredPermission
+        self.channelID = channelID
+        
