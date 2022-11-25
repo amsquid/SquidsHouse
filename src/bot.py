b89e4f308a7bd363109417062d039dfb73b94d45
Supercrazysquid <supercrazysquid@gmail.com>
2022-11-24 19:43:51 -0500

    Added notifications and default rooms

--- src/bot.py
+++ src/bot.py
@@ -26,16 +26,23 @@ token = config['token']
 rooms = []
 
 main_guild = 1041055949571498036
+notifications_channel = 1041056231986569306
+
+default_room = None
 
 # Setup discord bot
 client = discord.Client(intents=discord.Intents.all())
 tree = discord.app_commands.tree.CommandTree(client)
 
 # Functions
-def create_room(name, permission):
-	newRoom = Room(name, permission)
+def create_room(name, permission, default=False):
+	global default_room
 
+	newRoom = Room(name, permission)
 	rooms.append(newRoom)
+
+	if default:
+		default_room = newRoom
 
 # Commands
 @tree.command(guild=discord.Object(id=main_guild), name='rooms', description='Sends all the rooms')
@@ -50,7 +57,7 @@ async def _rooms(interaction: discord.Interaction):
 @tree.command(guild=discord.Object(id=main_guild), name='gotoroom', description='Sends user to a different room')
 async def _gotoroom(interaction: discord.Interaction, room: str):	
 	roles = []
-	author = interaction.user
+	member = interaction.user
 
 	room = room.lower()
 
@@ -62,24 +69,35 @@ async def _gotoroom(interaction: discord.Interaction, room: str):
 			gotoRoom = roomClass
 
 		roleId = roomClass.requiredPermission
-		roles.append(discord.utils.get(author.guild.roles, id=roleId))
+		roles.append(discord.utils.get(member.guild.roles, id=roleId))
 
 	# Getting group needed for room
 	roomPermission = gotoRoom.requiredPermission
-	roomRole = discord.utils.get(author.guild.roles, id=roomPermission)
+	roomRole = discord.utils.get(member.guild.roles, id=roomPermission)
 
 	# Clearing room permissions
 	for role in roles:
-		await author.remove_roles(role)
+		await member.remove_roles(role)
 
 	# Adding the main room's permissions
-	await author.add_roles(roomRole)
+	await member.add_roles(roomRole)
 
 	# Sending response
-	await interaction.response.send_message(author.display_name + ' has left the room')
+	await interaction.response.send_message(member.display_name + ' has left the room')
+
+@tree.command(guild=discord.Object(id=main_guild), name='notification', description="Sends a notification to all user's phones")
+async def _notification(interaction: discord.Interaction, message: str):
+	member = interaction.user
+
+	if member.guild_permissions.administrator:
+		await client.get_channel(notifications_channel).send('**NEW NOTIFICATION**\n' + message)
+
+		await interaction.response.send_message('Sent message!')
+	else:
+		await interaction.response.send_message('You cannot send notifications')
 
 # Setting up rooms
-create_room('livingroom', 1041057800090038393)
+create_room('livingroom', 1041057800090038393, True)
 create_room('kitchen',    1041057809225240686)
 create_room('bedroom',    1041057809946652792)
 
@@ -93,6 +111,16 @@ async def on_ready():
 	await tree.sync(guild=discord.Object(id=main_guild))
 
 	print(client.user.name, 'is ready!')
+
+@client.event
+async def on_member_join(member: discord.Member):
+	# Getting needed permission
+	permission = default_room.requiredPermission
+
+	# Adding user to that permission
+	role = discord.utils.get(member.guild.roles, id=permission)
+
+	await member.add_roles(role)
 
 # Bot login
 client.run(token)
